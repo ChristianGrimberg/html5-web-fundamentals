3c9b8ad4f031ca9ba9e227eedad5b50d4a680d06
Christian Grimberg <christian.grimberg@outlook.com>
2021-08-28 23:12:48 -0300

    :+1: Practica de formularios con el modulo 3.

--- wwwroot/php/modulo3.php
+++ wwwroot/php/modulo3.php
@@ -1,3 +1,8 @@
 <?php
+    $user = $_GET["name"];
+    $email = $_GET["email"];
+
     print '<a href="../class3-practice.html" target="_self">Atras</a><br>';
+    print '<h3>Bienvenid@ '.$user.'</h3>';
+    print '<p>Correo ingresado: '.$email.'</p>'
 ?>
