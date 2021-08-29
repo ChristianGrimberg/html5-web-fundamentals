15b8cc394c34b281ed5f7b8a3b5379df99023039
Christian Grimberg <christian.grimberg@outlook.com>
2021-08-29 14:19:19 -0300

    :+1: Se cambia los metodos HTTP y se prueban con atributos adicionales en el formulario.

--- wwwroot/php/modulo3.php
+++ wwwroot/php/modulo3.php
@@ -1,6 +1,6 @@
 <?php
-    $user = $_GET["name"];
-    $email = $_GET["email"];
+    $user = $_POST["name"];
+    $email = $_POST["email"];
 
     print '<a href="../class3-practice.html" target="_self">Atras</a><br>';
     print '<h3>Bienvenid@ '.$user.'</h3>';
