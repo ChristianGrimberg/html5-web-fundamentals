ecffc7f98d39d0cb3c6a5eb9c30ee5f94a7c1048
Christian Grimberg <christian.grimberg@outlook.com>
2021-08-29 14:59:55 -0300

    :+1: Practica de envio de valores a PHP.

--- wwwroot/php/modulo3.php
+++ wwwroot/php/modulo3.php
@@ -1,8 +1,14 @@
 <?php
     $user = $_POST["name"];
     $email = $_POST["email"];
+    $age = $_POST["age"];
+    $state = $_POST["state"];
+    $courses = $_POST["courses"];
 
     print '<a href="../class3-practice.html" target="_self">Atras</a><br>';
     print '<h3>Bienvenid@ '.$user.'</h3>';
-    print '<p>Correo ingresado: '.$email.'</p>'
+    print '<p>Correo ingresado: '.$email.'</p>';
+    print '<p>Edad: '.$age.'</p>';
+    print '<p>Estado Civil: '.$state.'</p>';
+    print '<p>Cursos: '.$courses.'</p>';
 ?>
